65da9e831dd3edd08c269c3b09201ab2e9d7ec97
Callum Dixon <callum.dixon@and.digital>
2022-06-21 11:59:33 +0100

    add VideoCallChecker Initial class

---
+++ TBD/VideoCallChecker.swift
@@ -0,0 +1,21 @@
+//
+//  VideoCallChecker.swift
+//  TBD
+//
+//  Created by Callum Dixon on 21/06/2022.
+//
+
+import Foundation
+import AVFoundation
+import CoreMediaIO
+
+class VideoCallChecker {
+    
+    var isInCall: Bool
+    
+    init(){
+        isInCall = true
+        print("Checking video")
+        
+    }
+}
